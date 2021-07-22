d596cbe312f845add1b9744e490c85616fda9e88
ADITYA KUMAR <onlyaditya001@gmail.com>
2021-07-22 09:23:42 +0530

    home page completed

---
+++ src/Components/Bottom.jsx
@@ -0,0 +1,148 @@
+import styled from "styled-components";
+import pink from "./images/pink.jpg";
+import rhombus from "./images/rhombus.png";
+import rhombus2 from "./images/rhombus2.png";
+
+const Wrapper = styled.div`
+  width: 70%;
+  margin: auto;
+  text-align: center;
+  font-family: sans-serif;
+
+  a {
+    color: #9c3353;
+    font-size: 22px;
+  }
+
+  p {
+    font-size: 28px;
+  }
+
+  p:nth-child(4) {
+    font-size: 22px;
+    color: gray;
+    word-spacing: 2mm;
+    line-height: 10mm;
+  }
+
+  h2 {
+    width: 100%;
+    text-align: center;
+    border-bottom: 1px solid #000;
+    line-height: 0.1em;
+    margin: 10px 0 20px;
+  }
+
+  h2 span {
+    padding: 0 10px;
+  }
+`;
+
+const Call = styled.div`
+  height: 100px;
+  background-image: url(${pink});
+  padding-top: 40px;
+
+  div {
+    width: 50%;
+    height: 70px;
+    margin: auto;
+    padding-left: 10%;
+    padding-right: 10%;
+    background: white;
+    border-radius: 8px;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  div > button {
+    width: 300px;
+    height: 50px;
+    margin-top: 10px;
+    border-radius: 40px;
+    border: none;
+    color: white;
+    background-color: #9c3353;
+    font-size: 18px;
+  }
+
+  div > button:hover {
+    cursor: pointer;
+  }
+
+  div > p {
+    font-size: 22px;
+  }
+`;
+
+const IconLine = styled.div`
+  display: flex;
+  text-align: center;
+
+  div {
+    width: 50%;
+    margin: 15px;
+    border-bottom: 1px solid #000;
+  }
+
+  img {
+    margin: 0 5px 0 5px;
+  }
+`;
+
+const Feed = styled.div`
+  width: 40%;
+  margin: 40px auto;
+  text-align: center;
+  font-family: sans-serif;
+
+  p {
+    font-size: 22px;
+  }
+`;
+const Bottom = () => {
+  return (
+    <>
+      <Wrapper>
+        <a>See More Fundraisers</a>
+
+        <p>A friend in your hour of need</p>
+        <IconLine>
+          <div></div>
+
+          <img src={rhombus2} alt="" />
+          <img src={rhombus} alt="" />
+          <img src={rhombus2} alt="" />
+
+          <div></div>
+        </IconLine>
+        <p>
+          Medical treatments are expensive. They can exhaust your insurance and
+          savings. With Milaap, you receive timely help to pay medical bills.
+          Here’s how:
+        </p>
+      </Wrapper>
+      <Call>
+        <div>
+          <p>Need help to setup your free fundraiser?</p>
+          <button>Request a Call</button>
+        </div>
+      </Call>
+
+      <Feed>
+        <p>What people are saying about Milaap</p>
+        <IconLine>
+          <div></div>
+
+          <img src={rhombus2} alt="" />
+          <img src={rhombus} alt="" />
+          <img src={rhombus2} alt="" />
+
+          <div></div>
+        </IconLine>
+      </Feed>
+    </>
+  );
+};
+
+export { Bottom };
---
+++ src/Components/Intro.jsx
@@ -0,0 +1,155 @@
+import styled from "styled-components";
+import next from "./images/next.png";
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const IntroWrapper = styled.div`
+  width: 35%;
+  margin: 100px 0px 0px 10%;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-family: sans-serif;
+    font-weight: 500;
+  }
+
+  h3 {
+    margin-top: -10px;
+    font-family: sans-serif;
+    font-weight: 550;
+    color: gray;
+  }
+
+  p {
+    font-size: 22px;
+  }
+
+  button {
+    width: 300px;
+    height: 50px;
+    border-radius: 40px;
+    border: none;
+    color: white;
+    background-color: #9c3353;
+    font-size: 18px;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+`;
+
+const CountWrapper = styled.div`
+  margin-top: 60px;
+  display: flex;
+  justify-content: space-between;
+
+  div {
+    height: 70px;
+    padding-right: 15px;
+    border-right: 1px solid #e0b9c5;
+  }
+
+  div > p {
+    font-size: 20px;
+  }
+
+  div > p:nth-child(1) {
+    margin-top: -1px;
+    color: #9c3353;
+  }
+
+  div > p:nth-child(2) {
+    margin-top: -15px;
+    color: gray;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  width: 40%;
+
+  img {
+    height: 550px;
+    width: 100%;
+    border-radius: 45% 0 45% 50%;
+  }
+`;
+
+const HeadBar = styled.div`
+  position: absolute;
+  top: 15%;
+  left: 25%;
+
+  button {
+    display: flex;
+    justify-content: space-between;
+    height: 50px;
+    font-size: 22px;
+    border: none;
+    box-shadow: 0 0 5px #9c3353;
+    border-radius: 5px;
+    padding: 0 25px 0 25px;
+    background-color: white;
+    color: gray;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  div {
+    margin-top: 10px;
+  }
+
+  img {
+    margin-top: 8px;
+    margin-left: 8px;
+  }
+`;
+const Intro = () => {
+  return (
+    <>
+      <HeadBar>
+        <button>
+          <div>9-Month-Old Khyati Need Your Help To Live An SMA Free Life</div>
+          <img src={next} alt="" />
+        </button>
+      </HeadBar>
+      <Wrapper>
+        <IntroWrapper>
+          <h1>Milaap</h1>
+          <h3>Free Crowdfunding for India</h3>
+          <p>Raise funds online for medical emergencies and social causes</p>
+          <button>Start a fundraiser - it's FREE</button>
+          <CountWrapper>
+            <div>
+              <p>40,000+</p>
+              <p>Fundraisers</p>
+            </div>
+            <div>
+              <p>Rs. 1400 Crores +</p>
+              <p>Raised</p>
+            </div>
+            <div>
+              <p>60 lakhs +</p>
+              <p>Donations</p>
+            </div>
+          </CountWrapper>
+        </IntroWrapper>
+        <ImageWrapper>
+          <img
+            src="https://s.yimg.com/ny/api/res/1.2/YGkApd3MpTUhALVM2NV_tA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTY0MA--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-images/2020-04/086dfa90-8155-11ea-bfff-66bdf3acb2fa"
+            alt=""
+          />
+        </ImageWrapper>
+      </Wrapper>
+    </>
+  );
+};
+
+export { Intro };
