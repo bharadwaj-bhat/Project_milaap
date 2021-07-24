76d5d3a503548ac06250dc9f6006d498218b06b4
Pulkit0111 <ptyagi1551@gmail.com>
2021-07-24 14:35:29 +0530

    fundraiser form updated and notification functionality added

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -2,6 +2,8 @@ import styles from "../Style.module.css";
 import axios from "axios";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function LoginForm({ handleLoggedIn }) {
   const [email, setEmail] = useState("");
@@ -11,11 +13,16 @@ function LoginForm({ handleLoggedIn }) {
     event.preventDefault();
   };
 
-  const handleHandler = (i) => {
-    handleLoggedIn(i);
-  };
+  // const handleHandler = (i) => {
+  //   handleLoggedIn(i);
+  // };
 
   let validation = false;
+  const notify = () => {
+    toast.error("Wrong Credentials, Please try again!", {
+      position: "top-center",
+    });
+  };
   const handleLogin = async () => {
     let { data } = await axios.get("http://localhost:3001/user");
     for (let i = 0; i < data.length; i++) {
@@ -30,7 +37,7 @@ function LoginForm({ handleLoggedIn }) {
       }
     }
     if (!validation) {
-      alert("Wrong Credentials!!");
+      notify();
     }
   };
 
@@ -41,36 +48,39 @@ function LoginForm({ handleLoggedIn }) {
     setPass(e.target.value);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div className={styles.field}>
-        <input
-          value={email}
-          type="text"
-          name="mobile"
-          id="mobile"
-          placeholder="Enter Mobile Number or Email"
-          onChange={handleEmail}
-        />
-        <label htmlFor="mobile">Mobile number / Email ID</label>
-      </div>
-      <div className={styles.field}>
-        <input
-          value={pass}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Enter Password"
-          onChange={handlePass}
-        />
-        <label htmlFor="password">Password</label>
-      </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.field}>
+          <input
+            value={email}
+            type="text"
+            name="mobile"
+            id="mobile"
+            placeholder="Enter Mobile Number or Email"
+            onChange={handleEmail}
+          />
+          <label htmlFor="mobile">Mobile number / Email ID</label>
+        </div>
+        <div className={styles.field}>
+          <input
+            value={pass}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Password"
+            onChange={handlePass}
+          />
+          <label htmlFor="password">Password</label>
+        </div>
 
-      {/* <Link to="/">
+        {/* <Link to="/">
       </Link> */}
-      <button className={styles.sign_up} onClick={handleLogin}>
-        Login
-      </button>
-    </form>
+        <button className={styles.sign_up} onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+      <ToastContainer />
+    </>
   );
 }
 
